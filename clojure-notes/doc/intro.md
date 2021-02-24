46c7ac6f66b07e19901d0164b6dbce743bbc9824
SofieStabell-UFST <74664088+SofieStabell-UFST@users.noreply.github.com>
2021-02-24 16:20:06 +0100

    Delete clojure-notes directory

--- clojure-notes/doc/intro.md
+++
@@ -1,3 +0,0 @@
-# Introduction to clojure-notes
-
-TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
--- clojure-notes/test/clojure_notes/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns clojure-notes.core-test
-  (:require [clojure.test :refer :all]
-            [clojure-notes.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
